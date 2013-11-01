fba1965e33ef2d498791e3140ea2cc9e4d0ae36f
Jeff Kile <21stcenturydigitaljeff@gmail.com>
2013-10-31 22:45:02 -0700

    Just starting on question1

---
+++ InterviewQuestions/week2/question1.js
@@ -0,0 +1,15 @@
+var fifoStack = [];
+var stackB = [];
+
+var push = function (pushA) {
+  fifoStack.push(pushMe); // Adds them to the end of the stack
+};
+
+var pop2B = function () {
+  for(var i=0; i<fifoStack.length; i++) {
+    stackB[i] = fifoStack[i];
+  }
+};
+
+
+
