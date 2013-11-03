0de6f84c3f6265660018f73ea27e277acd76305a
Jeff Kile <21stcenturydigitaljeff@gmail.com>
2013-11-02 22:56:29 -0700

    Created a working shell sort algorithm

---
+++ InterviewQuestions/week2/Intersection-of-two-sets/Intersection.java
@@ -0,0 +1,6 @@
+class Intersection {
+  public static void main(String[] args) {
+    int[] sortMe = {1,3,4,2,5,6, 8, 12, 4, -5, 1, 2, 5, 77, 5, 34, 2, 5, 74, 4, 34, 23, 3, 4, 6, 6, 4, 34, 5, 34, 2, 1, 4 ,6, 4, 8};
+    ShellSort.sort( sortMe );
+  }
+}
