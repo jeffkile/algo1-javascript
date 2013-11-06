2e12ef5623c46125121a1c21de554928abd9cdff
Jeff Kile <21stcenturydigitaljeff@gmail.com>
2013-11-06 13:13:56 -0800

    Got quicksort working

--- InterviewQuestions/week3/NutsAndBolts/Start.java
+++ InterviewQuestions/week3/NutsAndBolts/Start.java
@@ -1,23 +1,41 @@
-B E D C A
+class Start {
+  public static void main (String [] args) {
+    int[] sortMe = new int[] {1,3,4,2,5,3,2,5,6,8,6,7,4,3,6,9,9,0,2};
+    QuickSort.sort(sortMe);
+    prettyPrint(sortMe);
+  }
 
-5 2 3 1 4
-
-
-D
-
-Fits 5
-Bigger 1 4
-Smaller 2 3
-
-
-
-char[] nuts = new char[] {'B', 'E', 'D', 'C', 'A'};
-int[] bolts = new int[] {5, 2, 3, 1, 4};
-
-for (int j=0; j<bolts.length; j++) {
-
-  if(bigger(
-          for (int i=0; i<nuts.length; i++) {
+  private static void prettyPrint(int[] printMe) {
+    for(int i=0; i<printMe.length; i++) {
+      System.out.print(printMe[i] + " ");
+    }
+  }
+}
 
 
-public static void 
+
+
+
+// B E D C A
+
+// 5 2 3 1 4
+
+
+// D
+
+// Fits 5
+// Bigger 1 4
+// Smaller 2 3
+
+
+
+// char[] nuts = new char[] {'B', 'E', 'D', 'C', 'A'};
+// int[] bolts = new int[] {5, 2, 3, 1, 4};
+
+// for (int j=0; j<bolts.length; j++) {
+
+//   if(bigger(
+//           for (int i=0; i<nuts.length; i++) {
+
+
+// public static void 
